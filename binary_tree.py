bbaab07a6082ee39459664c8baa79fad7724d965
Madhubalatn9 <madhubalatn9@gmail.com>
2026-08-16 13:29:10 +0530

    Added remove method

---
+++ binary_tree.py
@@ -0,0 +1,63 @@
+class Tree:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+class BinaryTree:
+    def __init__(self):
+        self.root=None
+    def add(self,data):
+        if not self.root:
+            self.root=Tree(data)
+            return
+        self.recAdd(data,self.root)
+    def recAdd(self,data,node):
+        if not node.left:
+            node.left=Tree(data)
+        elif not node.right:
+            node.right=Tree(data)
+        else:
+            self.recAdd(data,node.left)
+    def display(self,depth=0,node=None):
+        if not node:
+            node=self.root
+        print(" "*depth,node.data)
+        if node.left:
+            self.display(depth+1,node.left)
+        if node.right:
+            self.display(depth+1,node.right)
+    def remove(self,data):
+        if not self.root:
+            print("Tree is empty")
+            return
+        if self.root.data==data:
+            self.root=None
+            return
+        self.recRemove(data,self.root)
+    def recRemove(self,data,node):
+        if node.left and node.left.data==data:
+            node.left=None
+        if node.right and node.right.data==data:
+            node.right=None
+
+        if node.left:
+            self.recRemove(data,node.left)
+        if node.right:
+            self.recRemove(data,node.right)
+
+       
+bt=BinaryTree()
+bt.add(1)
+bt.add(2)
+bt.add(3)
+bt.add(4)
+bt.add(5)
+bt.add(6)
+bt.add(7)
+bt.display()
+bt.remove(4)
+print("After remove element:")
+bt.display()
+
+    
