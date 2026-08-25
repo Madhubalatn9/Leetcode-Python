3f2cb0a51ea406ec6fdd851264404d60d485772a
Madhubalatn9 <madhubalatn9@gmail.com>
2026-08-25 14:58:30 +0530

    Added

--- smallest_missing.py
+++ smallest_missing.py
@@ -1,14 +1,20 @@
-nums = [8,2,3,4,6]
-k = 2
-num_set=set(nums)
-print(num_set)
-i=1
 
 
-while True:
-    mul=i*k
-    
-    if mul not in num_set:
-        print(mul)
-        break
-    i+=1
+
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        i=1
+
+
+        while True:
+            mul=i*k
+            
+            if mul not in nums:
+            
+                return mul
+            i+=1
+
+obj=Solution()
+
+obj1=obj.missingMultiple(3)
+print(obj.missingMultiple(3))
