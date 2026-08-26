149117d3d6d5bd4a80b9c27bfd02559c532a65a5
Madhubalatn9 <madhubalatn9@gmail.com>
2026-08-26 23:55:57 +0530

    remove_elements_from_list

---
+++ remove_element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+       
+        count=0
+        for i in range(len(nums)):
+            if(nums[i]!=val):
+                nums[count]=nums[i]
+                count+=1
+    
+        return count
+
+obj=Solution()
+print(obj.removeElement([3,2,2,3],3))
+        
+            
+                
+
+
